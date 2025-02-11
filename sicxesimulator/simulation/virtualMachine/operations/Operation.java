6c5bcc39c116cc6e3aa77d8fd5b6478bce5081eb
Renan Pinho <pinhorenan@icloud.com>
2025-02-11 19:19:50 -0300

    Criação de classe auxiliar Operation.java. Provavelmente temporária.

---
+++ sicxesimulator/simulation/virtualMachine/operations/Operation.java
@@ -0,0 +1,23 @@
+package sicxesimulator.simulation.virtualMachine.operations;
+
+import sicxesimulator.simulation.virtualMachine.Register;
+import sicxesimulator.simulation.virtualMachine.Word;
+
+public abstract class Operation {
+
+    public Operation(String op) {
+        // TODO
+    }
+
+    public Operation(Register r1, Register r2) {
+        // TODO
+    }
+
+    public Operation(Word op) {
+        // TODO
+    }
+
+    public Operation() {
+        // TODO DEFAULT PLACEHOLDER
+    }
+}

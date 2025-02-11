77eccbc784a1c62f437c8be1fbab8b006571a2d3
Renan Pinho <pinhorenan@icloud.com>
2025-02-11 20:18:02 -0300

    Limpeza de código.

--- sicxesimulator/simulation/virtualMachine/Word.java
+++ sicxesimulator/simulation/virtualMachine/Word.java
@@ -6,17 +6,10 @@ public class Word {
     // TODO
     // Deixar de trabalhar com inteiros e achar uma representação melhor para os bits na palavra.
 
-    /**
-     * Construtor padrão.
-     */
     public Word() {
         this.value = "";
     }
 
-    /**
-     * Construtor com valor.
-     * @param value - String conteúdo.
-     */
     public Word(String value) {
         this.value = value;
     }

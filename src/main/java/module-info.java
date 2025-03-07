617cfada89ab7b83514a64356f8bd3aea9ab53d5
Renan Pinho <pinhorenan@icloud.com>
2025-03-07 11:17:45 -0300

    Poderosíssimo.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,10 @@ module sicxesimulator {
     requires javafx.media;
     requires jdk.unsupported.desktop;
     requires java.logging;
+    requires org.junit.jupiter.api;
+    requires org.junit.platform.suite.api;
 
+    // Só exporta o(s) pacote(s) que o seu código de teste (ou outra aplicação) precisa enxergar
     exports sicxesimulator.simulator.view to javafx.graphics;
+    exports sicxesimulator.machine; // por ex., se o teste acessar classes neste pacote
 }
---
+++ src/test/java/module-info.java
@@ -0,0 +1,6 @@
+
+odule sicxesimulator.test {
+    requires sicxesimulator;
+    requires org.junit.jupiter.api;
+    requires org.junit.platform.suite.api;
+}

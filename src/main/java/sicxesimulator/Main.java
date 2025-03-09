ea96f8cbbe7c1f139137e66f66808400acfbbf42
Renan Pinho <pinhorenan@icloud.com>
2025-03-08 21:08:10 -0300

    Atualizando a classe Main para fazer a injeção de um modelo na aplicação.

--- src/main/java/sicxesimulator/Main.java
+++ src/main/java/sicxesimulator/Main.java
@@ -1,10 +1,14 @@
 package sicxesimulator;
 
 import javafx.application.Application;
+import sicxesimulator.simulator.model.MainModel;
 import sicxesimulator.simulator.view.MainApp;
 
 public class Main {
     public static void main(String[] args) {
+        MainModel model = new MainModel();  // Crie e configure seu Model aqui
+        MainApp.setModel(model);           // Injete o Model na MainApp
         Application.launch(MainApp.class, args);
     }
 }
+

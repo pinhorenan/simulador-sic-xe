950f5bf4c8366332b5efc42671324fe63fce0e4e
Renan Pinho <pinhorenan@icloud.com>
2025-03-10 01:38:27 -0300

    Atualização do module-info.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,26 +1,25 @@
 module sicxesimulator {
-    // Dependências de produção
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.media;
-    requires jdk.unsupported.desktop;
     requires java.logging;
 
-    // Exporta apenas o(s) pacote(s) que deve(m) ser acessível(is) por
-    // outros módulos ou pelo JavaFX.
-    // "mainView" exportado para o módulo javafx.graphics, por exemplo, se houver FXML que precise de acesso.
-    exports sicxesimulator.simulator.view to javafx.graphics;
-    exports sicxesimulator.simulator.view.components to javafx.graphics;
-    // Exemplo de pacote exportado sem restrição:
-    // (Se o teste ou outra parte do código no mesmo módulo precisa enxergar "machine",
-    //  e/ou se você quer que seja público para quem usar seu módulo.)
+    opens sicxesimulator.simulator.view to javafx.fxml, javafx.controls, javafx.media;
+    opens sicxesimulator.simulator.view.components to javafx.fxml, javafx.controls, javafx.media;
+
+    exports sicxesimulator.simulator.view;
+    exports sicxesimulator.simulator.view.components;
+    exports sicxesimulator.simulator.model;
+    exports sicxesimulator.simulator.controller;
     exports sicxesimulator.machine;
     exports sicxesimulator.linker;
     exports sicxesimulator.loader;
-    exports sicxesimulator.macroprocessor;
     exports sicxesimulator.assembler;
-    exports sicxesimulator.assembler.models;
+    exports sicxesimulator.macroprocessor;
     exports sicxesimulator.utils;
-    exports sicxesimulator.simulator.model;
-    exports sicxesimulator.simulator.controller;
+    exports sicxesimulator.models;
+    exports sicxesimulator.simulator.view.records;
+    opens sicxesimulator.simulator.view.records to javafx.controls, javafx.fxml, javafx.media;
+    exports sicxesimulator.simulator.view.components.tables;
+    opens sicxesimulator.simulator.view.components.tables to javafx.controls, javafx.fxml, javafx.media;
 }

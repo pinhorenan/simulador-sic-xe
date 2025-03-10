98da89903054920d83b0ba67fca8ffb669f1d052
Renan Pinho <pinhorenan@icloud.com>
2025-03-10 01:43:19 -0300

    Listener para o model.

--- src/main/java/sicxesimulator/simulator/model/ModelListener.java
+++ src/main/java/sicxesimulator/simulator/model/ModelListener.java
@@ -1,6 +1,5 @@
 package sicxesimulator.simulator.model;
 
-// Na interface ModelListener
 public interface ModelListener {
-    void onFilesUpdated();  // Método chamado quando os arquivos forem atualizados
+    void onFilesUpdated();
 }

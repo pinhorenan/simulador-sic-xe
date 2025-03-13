ea9df307e26c05be2d910670b0030869bb325dea
Renan Pinho <pinhorenan@icloud.com>
2025-03-12 23:47:15 -0300

    Adição de método de mapeamento de número->nome e nome->número de registradores.

--- src/main/java/sicxesimulator/utils/Map.java
+++ src/main/java/sicxesimulator/utils/Map.java
@@ -20,11 +20,35 @@ public abstract class Map {
     }
 
     /**
-     * Retorna o registrador conforme o número indicado.
-     * @param num Número do registrador (0 a 5).
-     * @return Registrador correspondente.
+     * Converte o nome do registrador para seu número correspondente.
+     * Exemplo: "A" -> 0, "X" -> 1, "L" -> 2, "B" -> 3, "S" -> 4, "T" -> 5.
+     *
+     * @param regName Nome do registrador (case insensitive).
+     * @return Número do registrador.
      */
-    public static Register numberToRegister(int num, RegisterSet registers) {
+    public static int registerNameToNumber(String regName) {
+        return switch (regName.toUpperCase()) {
+            case "A" -> 0;
+            case "X" -> 1;
+            case "L" -> 2;
+            case "B" -> 3;
+            case "S" -> 4;
+            case "T" -> 5;
+            case "F" -> 6;
+            case "PC" -> 8;
+            case "SW" -> 9;
+            default -> throw new IllegalArgumentException("Registrador inválido: " + regName);
+        };
+    }
+
+    /**
+     * Retorna o objeto Register correspondente ao número fornecido, usando o conjunto de registradores.
+     *
+     * @param num Número do registrador (0 a 5).
+     * @param registers Conjunto de registradores.
+     * @return O objeto Register correspondente.
+     */
+    public static Register getRegisterByNumber(int num, RegisterSet registers) {
         return switch (num) {
             case 0 -> registers.getRegister("A");
             case 1 -> registers.getRegister("X");
@@ -32,6 +56,9 @@ public abstract class Map {
             case 3 -> registers.getRegister("B");
             case 4 -> registers.getRegister("S");
             case 5 -> registers.getRegister("T");
+            case 6 -> registers.getRegister("F");
+            case 8 -> registers.getRegister("PC");
+            case 9 -> registers.getRegister("SW");
             default -> throw new IllegalArgumentException("Registrador inválido: " + num);
         };
     }
